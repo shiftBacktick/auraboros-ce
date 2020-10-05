70f713b45fce1378d2148a055deb96e49620ca4b
nicross <nicross@users.noreply.github.com>
2020-10-05 12:10:37 -0500

    Adjust dynamic range

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,9 @@ engine.ready(() => {
   engine.audio.mixer.master.param.limiter.attack.value = 0.003
   engine.audio.mixer.master.param.limiter.gain.value = 1
   engine.audio.mixer.master.param.limiter.knee.value = 15
-  engine.audio.mixer.master.param.limiter.ratio.value = 15
+  engine.audio.mixer.master.param.limiter.ratio.value = 20
   engine.audio.mixer.master.param.limiter.release.value = 0.125
-  engine.audio.mixer.master.param.limiter.threshold.value = -30
+  engine.audio.mixer.master.param.limiter.threshold.value = -36
 
   engine.audio.mixer.auxiliary.reverb.setImpulse(engine.audio.buffer.impulse.small())
   engine.audio.mixer.auxiliary.reverb.setGain(engine.utility.fromDb(-3))
